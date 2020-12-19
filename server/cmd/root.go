f6c91ceac52923bc78eeefec351d49915299f74e
git <31526284+cxqlkk@users.noreply.github.com>
2020-12-19 14:56:36 +0800

    init clinet

--- server/cmd/root.go
+++ server/cmd/root.go
@@ -31,34 +31,8 @@ var rootCmd = &cobra.Command{
 	Use:   "trains",
 	Short: "trains is the server of tranin (https://github.com/fatedier/frp)",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		if showVersion {
-			fmt.Println(version.Full())
-			return nil
-		}
-
-		var cfg config.ServerCommonConf
-		var err error
-		if cfgFile != "" {
-			var content string
-			content, err = config.GetRenderedConfFromFile(cfgFile)
-			if err != nil {
-				return err
-			}
-			cfg, err = parseServerCommonCfg(CfgFileTypeIni, content)
-		} else {
-			cfg, err = parseServerCommonCfg(CfgFileTypeCmd, "")
-		}
-		if err != nil {
-			return err
-		}
-
-		err = runServer(cfg)
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
 		return nil
-	},
+	}
 }
 
 func Execute() {
@@ -67,85 +41,3 @@ func Execute() {
 	}
 }
 
-func parseServerCommonCfg(fileType int, content string) (cfg config.ServerCommonConf, err error) {
-	if fileType == CfgFileTypeIni {
-		cfg, err = parseServerCommonCfgFromIni(content)
-	} else if fileType == CfgFileTypeCmd {
-		cfg, err = parseServerCommonCfgFromCmd()
-	}
-	if err != nil {
-		return
-	}
-
-	err = cfg.Check()
-	if err != nil {
-		return
-	}
-	return
-}
-
-func parseServerCommonCfgFromIni(content string) (config.ServerCommonConf, error) {
-	cfg, err := config.UnmarshalServerConfFromIni(content)
-	if err != nil {
-		return config.ServerCommonConf{}, err
-	}
-	return cfg, nil
-}
-
-func parseServerCommonCfgFromCmd() (cfg config.ServerCommonConf, err error) {
-	cfg = config.GetDefaultServerConf()
-
-	cfg.BindAddr = bindAddr
-	cfg.BindPort = bindPort
-	cfg.BindUDPPort = bindUDPPort
-	cfg.KCPBindPort = kcpBindPort
-	cfg.ProxyBindAddr = proxyBindAddr
-	cfg.VhostHTTPPort = vhostHTTPPort
-	cfg.VhostHTTPSPort = vhostHTTPSPort
-	cfg.VhostHTTPTimeout = vhostHTTPTimeout
-	cfg.DashboardAddr = dashboardAddr
-	cfg.DashboardPort = dashboardPort
-	cfg.DashboardUser = dashboardUser
-	cfg.DashboardPwd = dashboardPwd
-	cfg.LogFile = logFile
-	cfg.LogLevel = logLevel
-	cfg.LogMaxDays = logMaxDays
-	cfg.SubDomainHost = subDomainHost
-	cfg.TLSOnly = tlsOnly
-
-	// Only token authentication is supported in cmd mode
-	cfg.ServerConfig = auth.GetDefaultServerConf()
-	cfg.Token = token
-	if len(allowPorts) > 0 {
-		// e.g. 1000-2000,2001,2002,3000-4000
-		ports, errRet := util.ParseRangeNumbers(allowPorts)
-		if errRet != nil {
-			err = fmt.Errorf("Parse conf error: allow_ports: %v", errRet)
-			return
-		}
-
-		for _, port := range ports {
-			cfg.AllowPorts[int(port)] = struct{}{}
-		}
-	}
-	cfg.MaxPortsPerClient = maxPortsPerClient
-
-	if logFile == "console" {
-		cfg.LogWay = "console"
-	} else {
-		cfg.LogWay = "file"
-	}
-	cfg.DisableLogColor = disableLogColor
-	return
-}
-
-func runServer(cfg config.ServerCommonConf) (err error) {
-	log.InitLog(cfg.LogWay, cfg.LogFile, cfg.LogLevel, cfg.LogMaxDays, cfg.DisableLogColor)
-	svr, err := server.NewService(cfg)
-	if err != nil {
-		return err
-	}
-	log.Info("start frps success")
-	svr.Run()
-	return
-}
